0a8a6c56f649c5ae0b267ee31115d4487176221f
Matías Irland <mirland@xmartlabs.com>
2018-07-04 20:14:08 -0300

    Add Fountain kdoc documentation of public classes

--- fountain/src/main/java/com/xmartlabs/fountain/ListResponse.kt
+++ fountain/src/main/java/com/xmartlabs/fountain/ListResponse.kt
@@ -1,13 +1,19 @@
 package com.xmartlabs.fountain
 
+/** Represent a list service response. */
 interface ListResponse<T> {
+  /** Returns the response elements. */
   fun getElements(): List<T>
 }
 
+/** Represent a list service response that returns the total entity count. */
 interface ListResponseWithEntityCount<T> : ListResponse<T> {
-  fun getEntityCount() : Long
+  /** Returns the total entity count from the service. */
+  fun getEntityCount(): Long
 }
 
+/** Represent a list service response that returns the total page count. */
 interface ListResponseWithPageCount<T> : ListResponse<T> {
+  /** Returns the total page count from the service. */
   fun getPageCount(): Long
 }
