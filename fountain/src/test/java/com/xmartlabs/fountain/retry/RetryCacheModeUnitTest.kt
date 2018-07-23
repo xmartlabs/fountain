d5643425c6c055b7ea7c03e8071de73664c634cf
Matías Irland <matiasirland@gmail.com>
2018-07-23 12:02:35 -0300

    Fix issue when the initialization process throws an Exception (#29)

--- fountain/src/test/java/com/xmartlabs/fountain/retry/RetryCacheModeUnitTest.kt
+++ fountain/src/test/java/com/xmartlabs/fountain/retry/RetryCacheModeUnitTest.kt
@@ -1,10 +1,64 @@
 package com.xmartlabs.fountain.retry
 
 import com.xmartlabs.fountain.ListResponse
+import com.xmartlabs.fountain.NetworkState
+import com.xmartlabs.fountain.Status
 import com.xmartlabs.fountain.common.IntMockedListingCreator
 import com.xmartlabs.fountain.common.MockedNetworkDataSourceAdapter
+import com.xmartlabs.fountain.common.extensions.generateIntPageResponseList
+import com.xmartlabs.fountain.common.extensions.getPagedList
+import com.xmartlabs.fountain.common.extensions.mockLifecycleEvents
+import com.xmartlabs.fountain.common.extensions.sendPageResponse
+import org.junit.Assert
+import org.junit.Test
 
 class RetryCacheModeUnitTest : RetryUnitTest() {
   override fun createListing(mockedNetworkDataSourceAdapter: MockedNetworkDataSourceAdapter<ListResponse<Int>>) =
       IntMockedListingCreator.createNetworkWithCacheSupportListing(mockedNetworkDataSourceAdapter)
+
+  @Test
+  fun testRetryFirstCallWithDataSourceSaveError() {
+    val mockedNetworkDataSourceAdapter = MockedNetworkDataSourceAdapter<ListResponse<Int>>()
+    val listing = IntMockedListingCreator.createNetworkWithCacheSupportListing(mockedNetworkDataSourceAdapter, 2)
+        .mockLifecycleEvents()
+
+    Assert.assertEquals(NetworkState.LOADING, listing.networkState.value)
+
+    mockedNetworkDataSourceAdapter.sendPageResponse()
+    Assert.assertEquals(Status.FAILED, listing.networkState.value!!.status)
+
+    listing.retry.invoke()
+    Assert.assertEquals(NetworkState.LOADING, listing.networkState.value)
+    mockedNetworkDataSourceAdapter.sendPageResponse()
+    Assert.assertEquals(Status.FAILED, listing.networkState.value!!.status)
+
+    listing.retry.invoke()
+    Assert.assertEquals(NetworkState.LOADING, listing.networkState.value)
+    mockedNetworkDataSourceAdapter.sendPageResponse()
+    Assert.assertEquals(NetworkState.LOADED, listing.networkState.value)
+    Assert.assertEquals(generateIntPageResponseList(0), listing.getPagedList())
+  }
+
+  @Test
+  fun testRetryFirstCallWithDataSourceSaveErrorAndRetryError() {
+    val mockedNetworkDataSourceAdapter = MockedNetworkDataSourceAdapter<ListResponse<Int>>()
+    val listing = IntMockedListingCreator.createNetworkWithCacheSupportListing(mockedNetworkDataSourceAdapter, 2)
+        .mockLifecycleEvents()
+
+    Assert.assertEquals(NetworkState.LOADING, listing.networkState.value)
+
+    mockedNetworkDataSourceAdapter.sendPageResponse()
+    Assert.assertEquals(Status.FAILED, listing.networkState.value!!.status)
+
+    listing.retry.invoke()
+    Assert.assertEquals(NetworkState.LOADING, listing.networkState.value)
+    mockedNetworkDataSourceAdapter.sendPageResponse()
+    Assert.assertEquals(Status.FAILED, listing.networkState.value!!.status)
+
+    listing.retry.invoke()
+    Assert.assertEquals(NetworkState.LOADING, listing.networkState.value)
+    mockedNetworkDataSourceAdapter.sendPageResponse()
+    Assert.assertEquals(NetworkState.LOADED, listing.networkState.value)
+    Assert.assertEquals(generateIntPageResponseList(0), listing.getPagedList())
+  }
 }
